9258ed3c4b1b7e8b58370d73fdfb7565e60342f9
OthiRaja <121594447+Othi-Raja@users.noreply.github.com>
2023-10-24 11:45:06 +0530

    img

--- js.js
+++ js.js
@@ -41,20 +41,24 @@ window.addEventListener('load', () => {
   self.setInterval(() => {
     if (scrollDirection === 'forward') {
       if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
-        flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0);
+        flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0,{
+          behavior: "smooth"
+        });
         
       } else {
         scrollDirection = 'backward';
       }
     } else if (scrollDirection === 'backward') {
       if (flavoursContainer.scrollLeft !== 0) {
-        flavoursContainer.scrollTo(flavoursContainer.scrollLeft - 1, 0);
+        flavoursContainer.scrollTo(flavoursContainer.scrollLeft - 1, 0,{
+          behavior: "smooth"
+        });
       } else {
         scrollDirection = 'forward';
         
       }
     }
-  }, 50);
+  }, 200);
 });
 
  
