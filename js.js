d4c78d7f59714b07840cf42505355653ee16034a
OthiRaja <121594447+Othi-Raja@users.noreply.github.com>
2023-10-24 11:16:40 +0530

    img,bg

--- js.js
+++ js.js
@@ -41,50 +41,47 @@ window.addEventListener('load', () => {
   self.setInterval(() => {
     if (scrollDirection === 'forward') {
       if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
-        flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0);
-        
+        flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0,{
+          behavior: 'smooth',
+        });
+       
       } else {
         scrollDirection = 'backward';
       }
     } else if (scrollDirection === 'backward') {
       if (flavoursContainer.scrollLeft !== 0) {
-        flavoursContainer.scrollTo(flavoursContainer.scrollLeft - 1, 0);
+        flavoursContainer.scrollTo(flavoursContainer.scrollLeft - 1, 0,{
+          behavior: 'smooth',
+        });
+        scroll({
+          top: offsetTop,
+          behavior: "smooth"
+        });
       } else {
         scrollDirection = 'forward';
+        
       }
     }
-  }, 50);
+  },200);
 });
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-// create the smooth scroller FIRST!
-const smoother = ScrollSmoother.create({
-  content: "#scrollsmoother-container",
-  smooth: 3,
-  normalizeScroll: true,
-  ignoreMobileResize: true,
-	effects: true,
-  //preventDefault: true,
-  //ease: 'power4.out',
-  //smoothTouch: 0.1, 
-});
+ 
 
 function gform_link(){
-  window.open("https://forms.gle/wxr2rPHsnPm83g4u5");
-}
-const popup = document.querySelector(".popupBox");
-const close = document.querySelector(".close");
+  window.open("https://forms.gle/wxr2rPHsnPm83g4u5");}
+// }
+// const popup = document.querySelector(".popupBox");
+// const close = document.querySelector(".close");
 
-window.onload = function () {
-  setTimeout(() => {
-    popup.style.display = "block";
-  }, 3000);
-};
+// window.onload = function () {
+//   setTimeout(() => {
+//     popup.style.display = "block";
+//   }, 3000);
+// };
 
-close.addEventListener("click", () => {
-  popup.style.display = "none";
-});
+// close.addEventListener("click", () => {
+//   popup.style.display = "none";
+// });
 
 
 const load =document.querySelector(".loader");
