c504f6506fadd1d4586a6b5a4c3e4a2be38f9f49
OthiRaja <121594447+Othi-Raja@users.noreply.github.com>
2023-10-24 12:02:33 +0530

    link

--- js.js
+++ js.js
@@ -1,37 +1,4 @@
-// const flavoursContainer = document.getElementById('scroll-container');
-// const flavoursScrollWidth = flavoursContainer.scrollWidth;
 
-//  window.addEventListener('load', () => {
-//    self.setInterval(() => {
-//      if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
-//        flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0);
-//      }
-//    }, 15);
-//  });
-
-
-// const flavoursContainer = document.getElementById('scroll-container');
-// const flavoursScrollWidth = flavoursContainer.scrollWidth;
-
-// window.addEventListener('load', () => {
-//   let scrollDirection = 1;
-
-//   self.setInterval(() => {
-//     if (scrollDirection === 1) {
-//       if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
-//         flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0);
-//       } else {
-//         scrollDirection = -1;
-//       }
-//     } else {
-//       if (flavoursContainer.scrollLeft !== 0) {
-//         flavoursContainer.scrollTo(flavoursContainer.scrollLeft - 1, 0);
-//       } else {
-//         scrollDirection = 1;
-//       }
-//     }
-//   }, 15);
-// });
 const flavoursContainer = document.getElementById('scroll-container');
 const flavoursScrollWidth = flavoursContainer.scrollWidth;
 
